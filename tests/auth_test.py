45b154fd996def99ff464167b65866ee0b63d79b
Julian <jll38@njit.edu>
2022-05-07 02:24:58 -0400

    register test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,4 +1,6 @@
-
+from app.db.models import User
+from app.auth.forms import register_form
+from app.auth import register
 """This test the homepage"""
 
 def test_request_main_menu_links(client):
@@ -7,3 +9,9 @@ def test_request_main_menu_links(client):
     assert response.status_code == 200
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
+
+def test_register(client):
+    form = register_form
+    form.email.data = "test@testmail.com"
+    form.password.data = "password"
+    register()
