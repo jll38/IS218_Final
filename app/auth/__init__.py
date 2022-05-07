b8b0a560ce3ec4aca38e6425a4fc4ee5f5ef33d9
Julian <jll38@njit.edu>
2022-05-07 03:13:09 -0400

    login

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -54,3 +54,111 @@ def register():
 def dashboard():
     if not current_user.is_authenticated():
         return redirect(url_for('auth.login'))
+
+@auth.route("/logout")
+@login_required
+def logout():
+    """Logout the current user."""
+    user = current_user
+    user.authenticated = False
+    db.session.add(user)
+    db.session.commit()
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+@auth.route('/users')
+@login_required
+@admin_required
+def browse_users():
+    data = User.query.all()
+    titles = [('email', 'Email'), ('registered_on', 'Registered On')]
+    retrieve_url = ('auth.retrieve_user', [('user_id', ':id')])
+    edit_url = ('auth.edit_user', [('user_id', ':id')])
+    add_url = url_for('auth.add_user')
+    delete_url = ('auth.delete_user', [('user_id', ':id')])
+
+    current_app.logger.info("Browse page loading")
+
+    return render_template('browse.html', titles=titles, add_url=add_url, edit_url=edit_url, delete_url=delete_url,
+                           retrieve_url=retrieve_url, data=data, User=User, record_type="Users")
+
+
+@auth.route('/users/<int:user_id>')
+@login_required
+def retrieve_user(user_id):
+    user = User.query.get(user_id)
+    return render_template('profile_view.html', user=user)
+
+
+@auth.route('/users/<int:user_id>/edit', methods=['POST', 'GET'])
+@login_required
+def edit_user(user_id):
+    user = User.query.get(user_id)
+    form = user_edit_form(obj=user)
+    if form.validate_on_submit():
+        user.about = form.about.data
+        user.is_admin = int(form.is_admin.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('User Edited Successfully', 'success')
+        return redirect(url_for('auth.browse_users'))
+    return render_template('user_edit.html', form=form)
+
+
+@auth.route('/users/new', methods=['POST', 'GET'])
+@login_required
+def add_user():
+    form = register_form()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = User(email=form.email.data, password=generate_password_hash(form.password.data))
+            db.session.add(user)
+            db.session.commit()
+            flash('Congratulations, you just created a user', 'success')
+            return redirect(url_for('auth.browse_users'))
+        else:
+            flash('Already Registered')
+            return redirect(url_for('auth.browse_users'))
+    return render_template('user_new.html', form=form)
+
+
+@auth.route('/users/<int:user_id>/delete', methods=['POST'])
+@login_required
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user.id == current_user.id:
+        flash("You can't delete yourself!")
+        return redirect(url_for('auth.browse_users'), 302)
+    db.session.delete(user)
+    db.session.commit()
+    flash('User Deleted', 'success')
+    return redirect(url_for('auth.browse_users'), 302)
+
+
+@auth.route('/profile', methods=['POST', 'GET'])
+def edit_profile():
+    user = User.query.get(current_user.get_id())
+    form = profile_form(obj=user)
+    if form.validate_on_submit():
+        user.about = form.about.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('You Successfully Updated your Profile', 'success')
+        return redirect(url_for('auth.dashboard'))
+    return render_template('profile_edit.html', form=form)
+
+
+@auth.route('/account', methods=['POST', 'GET'])
+def edit_account():
+    user = User.query.get(current_user.get_id())
+    form = security_form(obj=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.password = form.password.data
+        db.session.add(current_user)
+        db.session.commit()
+        flash('You Successfully Updated your Password or Email', 'success')
+        return redirect(url_for('auth.dashboard'))
+    return render_template('manage_account.html', form=form)
