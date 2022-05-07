bf2eac2e3cf804d0f79f28890ecd2d53f8cf98a3
Julian <jll38@njit.edu>
2022-05-07 03:15:31 -0400

    login

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-
+from app.auth.decorators import admin_required
 from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash
 from app.auth.forms import login_form, register_form
@@ -8,6 +8,7 @@ from app.db.models import User
 
 auth = Blueprint('auth', __name__, template_folder='templates')
 from flask import current_app
+
 
 @auth.route('/login', methods=['POST', 'GET'])
 def login():
