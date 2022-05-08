c860e48e9c5d5034f6bc9a9ad0630f8b0394bb0c
Julian <jll38@njit.edu>
2022-05-08 19:11:34 -0400

    dashboard deny test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,6 +1,7 @@
 from app.db.models import User
 from app.auth.forms import register_form
 from app.auth import register
+from flask_login import logout_user
 """This test the homepage"""
 
 def test_request_main_menu_links(client):
@@ -10,8 +11,10 @@ def test_request_main_menu_links(client):
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
 
-def test_register(client):
-    form = register_form
-    form.email.data = "test@testmail.com"
-    form.password.data = "password"
-    register()
+def test_dashboard_deny(client):
+    """test dashboard access when not logged in"""
+    if (User.is_authenticated()):
+        logout_user()
+    response = client.get("/")
+    assert response.status_code == 302
+
