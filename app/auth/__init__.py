86cd03f03bb6af5de85c64ba8b36f96843e9514d
Julian <jll38@njit.edu>
2022-05-08 19:49:28 -0400

    login fix

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -16,7 +16,7 @@ def login():
     if(current_user.is_authenticated):
         return redirect(url_for('auth.dashboard'))
     if form.validate_on_submit():
-        user = User.query.filter_by(email=form.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if User is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('auth.login'))
