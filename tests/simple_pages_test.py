9510c53a93e4153856f8df16b37f207b4c8c8ded
Julian <jll38@njit.edu>
2022-05-08 18:43:49 -0400

    test page not found

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -14,3 +14,8 @@ def test_login_page(client):
     """login page test"""
     response = client.get("/login")
     assert response.status_code == 200
+
+def test_page_not_found(client):
+    """test 404 page not found"""
+    response = client.get("/page0")
+    assert response.status_code == 400
